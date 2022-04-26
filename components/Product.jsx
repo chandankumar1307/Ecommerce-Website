b2b1bbf99c2d603d77f324b8f4d5b62275aff170
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2022-04-26 23:36:59 +0530

    working on product card

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
-const Product = () => {
-  return <div>Product</div>;
+import { urlFor } from "../lib/client";
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card"></div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
