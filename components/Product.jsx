d6e8ee545f656b1f28bc454c43bdeafae20c3bb8
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2022-04-27 23:03:56 +0530

    products added

--- components/Product.jsx
+++ components/Product.jsx
@@ -7,7 +7,17 @@ const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
-        <div className="product-card"></div>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+            alt="image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
       </Link>
     </div>
   );
